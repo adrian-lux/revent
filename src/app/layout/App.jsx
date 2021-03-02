47222be5eaabe1ebd0b4faccd3b4581b98fd8840
Adrian <adorian@akita-itlabo.com>
2021-03-02 13:55:31 +0900

    end of section one

---
+++ src/app/layout/App.jsx
@@ -0,0 +1,13 @@
+import './styles.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Re-vent
+      </header>
+    </div>
+  );
+}
+
+export default App;
